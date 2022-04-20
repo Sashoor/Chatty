752ab75fc06c1c89bc84efcd9bed423e749fb17c
sashoor <saeedashoor.dev@gmail.com>
2022-04-20 18:46:20 +0100

    adding all my files

---
+++ my-app/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Sidebar from './Sidebar';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="App-header">
+        <div className="app__body">
+
+  <Sidebar />
+  {/* Chat */}
+
+</div>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAZ29Qxhc5hLP1UQefBQxO8dnzjpZfNLck",
+    authDomain: "chatty-c4d40.firebaseapp.com",
+    projectId: "chatty-c4d40",
+    storageBucket: "chatty-c4d40.appspot.com",
+    messagingSenderId: "944436772196",
+    appId: "1:944436772196:web:4da97d202ba57b53f7ce71"
+  };
